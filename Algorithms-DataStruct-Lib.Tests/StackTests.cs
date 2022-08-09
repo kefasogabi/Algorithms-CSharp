23de34010344f01ba12ccfc2e3b1000868b9796a
Kefas Ogabi <kefasogabi@gmail.com>
2022-08-09 23:54:50 +0100

    09/08/2023 0

---
+++ Algorithms-DataStruct-Lib.Tests/StackTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_DataStruct_Lib.Tests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        public void IsEmpty_EmptyStack_ReturnsTrue()
+        {
+            var stack = new ArrayStack<int>();
+            Assert.IsTrue(stack.IsEmpty);
+        }
+
+        [Test]
+        public void Count_PushOneItem_ReturnsOne()
+        {
+            var stack = new ArrayStack<int>();
+            stack.Push(1);
+
+            Assert.AreEqual(1, stack.Count);
+            Assert.IsFalse(stack.IsEmpty);
+        }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowException()
+        {
+            var stack = new ArrayStack<int>();
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                stack.Pop();
+            });
+        }
+
+        [Test]
+        public void Peek_PushTwoItems_ReturnsHeadItem()
+        {
+            var stack = new ArrayStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+
+            Assert.AreEqual(2, stack.Peek());
+        }
+
+        [Test]
+        public void Peek_PushTwoItemsAndPop_ReturnsHeadElement()
+        {
+            var stack = new ArrayStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+
+            stack.Pop();
+
+            Assert.AreEqual(1, stack.Peek());
+        }
+    }
+
+}
---
+++ Algorithms-DataStruct-Lib/ArrayStack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_DataStruct_Lib
+{
+    public class ArrayStack<T>
+    {
+        public bool IsEmpty { get; set; }
+        public int Count { get; set; }
+
+        private T[] _items;
+
+        public ArrayStack()
+        {
+            const int defaultCapacity = 4;
+            _items = new T[defaultCapacity];
+        }
+
+        public void Push(T p0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Pop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
