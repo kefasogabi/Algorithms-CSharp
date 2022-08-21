2792cf095a9bb966fa5dd9e832cfc7302276218c
Kefas Ogabi <kefasogabi@gmail.com>
2022-08-21 23:49:10 +0100

    21/08/2022 2

---
+++ Algorithms-DataStruct-Lib.Tests/CircularQueueTests.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_DataStruct_Lib.Tests
+{
+    [TestFixture]
+    public class CircularQueueTests
+    {
+        [Test]
+        public void Capacity_EnqueueManyItems_DoubledCapacity()
+        {
+            var queue = new CircularQueue<int>();
+
+            queue.Enqueue(1);
+            queue.Dequeue();
+
+            queue.Enqueue(2);
+            queue.Dequeue();
+
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+
+            Assert.AreEqual(8, queue.Capacity);
+        }
+
+        [Test]
+        public void IsEmpty_EmptyQueue_ReturnsTrue()
+        {
+            var queue = new CircularQueue<int>();
+
+            Assert.IsTrue(queue.IsEmpty);
+        }
+
+        [Test]
+        public void Count_EnqueueOneItem_ReturnsOne()
+        {
+            var queue = new CircularQueue<int>();
+            queue.Enqueue(1);
+
+            Assert.AreEqual(1, queue.Count);
+            Assert.IsFalse(queue.IsEmpty);
+        }
+
+        [Test]
+        public  void Dequeue_EmptyQueue_ThrowsException()
+        {
+            var queue = new CircularQueue<int>();
+
+            Assert.Throws<InvalidOperationException>(() => queue.Dequeue());
+        }
+
+        [Test]
+        public void Peek_EnqueueTwoItems_ReturnsHeadItem()
+        {
+            var queue = new CircularQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+
+            Assert.AreEqual(1, queue.Peek());
+        }
+
+        [Test]
+        public void Peek_EnqueueTwoItemsAndDequeue_ReturnsHeadElement()
+        {
+            var queue = new CircularQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+
+            queue.Dequeue();
+
+            Assert.AreEqual(2, queue.Peek());
+        }
+    }
+}
