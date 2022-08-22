ee79aff518bab4bf5760a3e751086f986b2ef8da
Kefas Ogabi <kefasogabi@gmail.com>
2022-08-22 22:18:38 +0100

    22/08/2022

---
+++ Algorithms-DataStruct-Lib/LinkedQueue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_DataStruct_Lib
+{
+    public class LinkedQueue<T> : IEnumerable<T>
+    {
+
+        private readonly SinglyLinkedList<T> _list = new SinglyLinkedList<T>();
+
+        public void Enqueue(T item)
+        {
+            _list.AddLast(item);    
+        }
+
+        public void Dequeue()
+        {
+            _list.RemoveFirst();
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException();
+
+            return _list.Head.Value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int Count => _list.Count;
+        public bool IsEmpty => Count == 0;
+
+
+    }
+}
